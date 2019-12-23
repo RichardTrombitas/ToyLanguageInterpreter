8cbf2aea22e2cded3de2511835726eb4cb8772dd
RichardTrombitas <richard.trombitas@yahoo.com>
2019-12-23 10:38:34 +0200

    *savegame*

--- src/View/GUI/MainCtrl.java
+++ src/View/GUI/MainCtrl.java
@@ -1,5 +1,15 @@
 package View.GUI;
 
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class MainCtrl {
+
+    public Button choosePrgButton;
+
+    public void handleChoosePrgButtonClick() throws IOException {
+        PrgSelectionCtrl.display();
+    }
 
 }
--- src/View/GUI/PrgSelectionCtrl.java
+++ src/View/GUI/PrgSelectionCtrl.java
@@ -1,6 +1,28 @@
 package View.GUI;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+import java.io.IOException;
+
 public class PrgSelectionCtrl {
+
+    public static void display() throws IOException {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Program selection");
+        Parent root = FXMLLoader.load(PrgSelectionCtrl.class.getResource("PrgSelection.fxml"));
+        window.setScene(new Scene(root));
+        window.showAndWait();
+
+
+    }
+
 }
 
 
